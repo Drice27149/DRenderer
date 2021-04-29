72e16a6bc00df9d1ca54527d9221dde0615ad3cd
eddardchen <43438296+Drice27149@users.noreply.github.com>
2021-04-29 21:30:08 +0800

    20210429_2

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -106,8 +106,7 @@ SkyBox::SkyBox(float up, float down, float left, float right, float front, float
 
 Frustum::Frustum(float fov, float aspect, float n, float f, int xCnt, int yCnt, int zCnt)
 {
-
-
+    
 }
 
 
